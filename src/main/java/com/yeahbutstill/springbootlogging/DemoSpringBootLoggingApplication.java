f77c1db3e4e40030508fd6425365120caf89e874
yeahbutstill <mayadani28@gmail.com>
2022-07-16 03:10:19 +0700

    setup

---
+++ src/main/java/com/yeahbutstill/springbootlogging/DemoSpringBootLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.yeahbutstill.springbootlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeahbutstill/springbootlogging/DemoSpringBootLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeahbutstill.springbootlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
