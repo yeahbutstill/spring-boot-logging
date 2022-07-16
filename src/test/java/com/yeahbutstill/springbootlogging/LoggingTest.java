0c01e16cc4dece1432d459e689b473690bd485bc
yeahbutstill <mayadani28@gmail.com>
2022-07-16 22:53:56 +0700

    Logging Level

--- src/test/java/com/yeahbutstill/springbootlogging/LoggingTest.java
+++ src/test/java/com/yeahbutstill/springbootlogging/LoggingTest.java
@@ -1,2 +1,21 @@
-package com.yeahbutstill.springbootlogging;public class LoggingTest {
+package com.yeahbutstill.springbootlogging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@Slf4j
+@SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
+public class LoggingTest {
+
+    @Test
+    void testLog() {
+        log.info("Log");
+        log.debug("log1");
+        log.error("log2");
+        log.trace("log3");
+        log.warn("log4");
+    }
 }
